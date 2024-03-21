a3c3c4965ff3b4be67e64eb4b472f4354c205e7b
AzhaQariCB <azhaqari@cueback.com>
2024-03-21 15:42:37 -0400

    theme updates and colored feed

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:eatery/screens/home_screen.dart';
 import 'package:eatery/screens/search_screen.dart';
 import 'package:eatery/screens/profile_screen.dart';
-import 'package:eatery/widgets/bottom_app_bar.dart';
+// import 'package:eatery/screens/signup_screen.dart';
+// import 'package:eatery/screens/login_screen.dart'; // Import the LoginPage
 import 'package:eatery/theme.dart';
 import 'package:algolia/algolia.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart'; // Import the Firebase options file
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
-      theme: myAppTheme, // Use the theme defined in theme.dart
-      initialRoute: '/',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.dark,
+      initialRoute: '/login', // Set initial route to LoginPage
       routes: {
-        '/': (context) => HomeScreen(),
+        '/home': (context) => HomeScreen(),
         '/search': (context) => SearchScreen(),
         '/profile': (context) => ProfileScreen(),
+        // '/signup': (context) => SignupPage(),
+        // '/login': (context) => LoginPage(), // Add LoginPage route
       },
     );
   }
