4130ba9a04f02629584b8f60513caf29fb8813fe
KyleSandersI <kisander@uci.edu>
2024-04-18 00:13:39 -0700

    added the correct date to the tracked meals

--- lib/meal_model.dart
+++ lib/meal_model.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Meal {
   final String name;
   final String restaurant;
   final int calories;
   final int protein;
   final int price;
+  final DateTime dateTracked; // Add this line
 
   Meal({
     required this.name,
@@ -11,18 +14,28 @@ class Meal {
     required this.calories,
     required this.protein,
     required this.price,
+    required this.dateTracked, // Add this line
   });
 
   factory Meal.fromMap(Map<String, dynamic> map) {
+    // Parsing the 'dateTracked' field correctly depending on its type
+    DateTime parsedDateTracked;
+    if (map['dateTracked'] is Timestamp) {
+      parsedDateTracked = (map['dateTracked'] as Timestamp).toDate();
+    } else if (map['dateTracked'] is String) {
+      parsedDateTracked = DateTime.parse(map['dateTracked']);
+    } else {
+      // Default to current date if parsing fails
+      parsedDateTracked = DateTime.now();
+    }
+
     return Meal(
       name: map['item name'] as String? ?? 'N/A',
       restaurant: map['restaurant'] as String? ?? 'N/A',
       calories: int.tryParse(map['calories']?.toString() ?? '0') ?? 0,
       protein: int.tryParse(map['protein']?.toString() ?? '0') ?? 0,
-      // Check if 'price' exists and is not an empty string; if it is, use default value 0
-      price: (map['0'] as String?)?.isEmpty ?? true
-          ? 0
-          : int.tryParse(map['price'] ?? '0') ?? 0,
+      price: int.tryParse(map['0']?.toString() ?? '0') ?? 0,
+      dateTracked: parsedDateTracked,
     );
   }
 }
--- lib/widgets/your_eats_widget.dart
+++ lib/widgets/your_eats_widget.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:eatery/widgets/meal_card.dart';
 import 'package:eatery/meal_model.dart';
+import 'package:intl/intl.dart';
 
 class YourEatsWidget extends StatelessWidget {
   @override
@@ -10,36 +11,67 @@ class YourEatsWidget extends StatelessWidget {
     final FirebaseAuth auth = FirebaseAuth.instance;
     final String? userId = auth.currentUser?.uid;
 
-    // Adjust the stream to listen to changes in the user's document
     return StreamBuilder<DocumentSnapshot>(
       stream: FirebaseFirestore.instance
           .collection('users')
           .doc(userId)
           .snapshots(),
       builder: (context, snapshot) {
-        if (!snapshot.hasData || snapshot.data == null) {
+        if (!snapshot.hasData) {
           return Center(child: CircularProgressIndicator());
         }
 
-        // Extract the trackedMeals array from the user's document
-        var trackedMealsData = snapshot.data!.get('trackedMeals') as List;
-        var meals = trackedMealsData
-            .map((mealData) => Meal.fromMap(mealData as Map<String, dynamic>))
-            .toList();
+        // Extract the trackedMeals array from the snapshot
+        var trackedMealsData =
+            snapshot.data!.get('trackedMeals') as List<dynamic>;
 
-        // If the array is empty, show a placeholder message
-        if (meals.isEmpty) {
-          return Center(child: Text("You haven't tracked any meals yet."));
+        // Convert the dynamic meals to a list of Meal objects
+        List<Meal> meals = trackedMealsData.map((mealData) {
+          return Meal.fromMap(mealData as Map<String, dynamic>);
+        }).toList();
+
+        // Group meals by the 'dateTracked' field
+        Map<String, List<Meal>> mealsGroupedByDate = {};
+        for (var meal in meals) {
+          // Format the date as 'day month, year'
+          String formattedDate =
+              DateFormat('dd MMMM, yyyy').format(meal.dateTracked);
+          mealsGroupedByDate.putIfAbsent(formattedDate, () => []).add(meal);
         }
 
+        // Convert the map into a list of map entries and sort by date
+        List<MapEntry<String, List<Meal>>> sortedMeals =
+            mealsGroupedByDate.entries.toList()
+              ..sort((a, b) =>
+                  b.key.compareTo(a.key)); // Display most recent dates first
+
         return ListView.builder(
-          itemCount: meals.length,
+          itemCount: sortedMeals.length,
           itemBuilder: (context, index) {
-            return MealCard(
-              meal: meals[index],
-              meals:
-                  meals, // Pass the entire list if needed for detail navigation
-              index: index,
+            var entry = sortedMeals[index];
+            var date = entry.key;
+            var mealsForDate = entry.value;
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    date, // Formatted date string
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ...mealsForDate.map((meal) {
+                  return MealCard(
+                    meal: meal,
+                    meals:
+                        mealsForDate, // Pass the list of meals for this date to the MealCard
+                    index: mealsForDate.indexOf(
+                        meal), // Pass the index of the meal in the list
+                  );
+                }).toList(),
+              ],
             );
           },
         );
